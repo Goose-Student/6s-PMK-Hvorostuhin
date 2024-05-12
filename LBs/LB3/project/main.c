e67e7c6ca9658997fd863118dc922ef2ba9875ac
Goose <49313915+Goose-Student@users.noreply.github.com>
2024-05-12 21:37:17 +0300

    Update 3lb

--- LBs/LB3/project/main.c
+++ LBs/LB3/project/main.c
@@ -12,6 +12,7 @@ static TIM_TimeBaseInitTypeDef TimObj;
 static char buffer;      // Буфер для хранения принятых данных
 static char xbuffer[64]; // Буфер для хранения массива данных
 static uint8_t xlen = 0; // Длина буфера
+static uint32_t counter = 0;
 
 // Функция вывода массива байтов
 static void sendArr(char *data)
@@ -104,7 +105,7 @@ static void initUSART(void) // Функция инициализации USART
     USART_InitTypeDef UsartObj;                            // Объявление структуры для инициализации USART
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); // Включение тактирования USART1
 
-    UsartObj.USART_BaudRate = 9600;                                      // Установка скорости передачи данных
+    UsartObj.USART_BaudRate = 2400;                                      // Установка скорости передачи данных
     UsartObj.USART_WordLength = USART_WordLength_8b;                     // Установка длины слова
     UsartObj.USART_StopBits = USART_StopBits_1;                          // Установка количества стоп-битов
     UsartObj.USART_Parity = USART_Parity_No;                             // Установка контроля четности
@@ -114,48 +115,8 @@ static void initUSART(void) // Функция инициализации USART
 
     USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // Включение прерывания по приему данных
     USART_Cmd(USART1, ENABLE);                     // Включение USART1
-}
-
-static void initNVIC(void) // Функция инициализации NVIC
-{
-    NVIC_InitTypeDef NvicObj;                      // Объявление структуры для инициализации NVIC
-    NvicObj.NVIC_IRQChannel = USART1_IRQn;         // Установка канала прерывания
-    NvicObj.NVIC_IRQChannelPreemptionPriority = 0; // Установка приоритета прерывания
-    NvicObj.NVIC_IRQChannelCmd = ENABLE;           // Включение канала прерывания
-    NvicObj.NVIC_IRQChannelSubPriority = 0;        // Установка подприоритета прерывания
-    NVIC_Init(&NvicObj);                           // Инициализация NVIC с использованием структуры Obj
-
-    NvicObj.NVIC_IRQChannel = TIM3_IRQn;           // Установка канала прерывания для TIM3
-    NvicObj.NVIC_IRQChannelPreemptionPriority = 1; // Установка приоритета прерывания
-    NvicObj.NVIC_IRQChannelCmd = ENABLE;           // Включение канала прерывания
-    NvicObj.NVIC_IRQChannelSubPriority = 1;        // Установка подприоритета прерывания
-    NVIC_Init(&NvicObj);                           // Инициализация NVIC с использованием структуры Obj для TIM3
-}
-
-static void initTIM3(void)
-{
-    /* Включение тактирования таймера TIM3 */
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-    /* Настройка периода и прескеллера TIM3 */
-    TimObj.TIM_Period = 255;
-    TimObj.TIM_Prescaler = 107;
-
-    /* Настройка делителя частоты, режим счета таймера TIM3 */
-    TimObj.TIM_ClockDivision = 0;                /* Делитель частоты таймера */
-    TimObj.TIM_CounterMode = TIM_CounterMode_Up; /* Режим счета вверх */
-    TIM_TimeBaseInit(TIM3, &TimObj);             /* Применение настроек */
-
-    /* Настройка канала 1 в режиме PWM */
-    ChObj.TIM_OCMode = TIM_OCMode_PWM1;             /* Режим работы канала - PWM1 */
-    ChObj.TIM_OutputState = TIM_OutputState_Enable; /* Включение выхода канала */
-    ChObj.TIM_OCPolarity = TIM_OCPolarity_High;     /* Полярность выходного сигнала - прямая */
-    ChObj.TIM_Pulse = TimObj.TIM_Period / 2;
-
-    TIM_OC1Init(TIM3, &ChObj);                 /* Применение настроек к каналу 1 таймера TIM3 */
-    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE); /* Включение прерывания на канале 1 */
-
-    TIM_Cmd(TIM3, ENABLE);
+    NVIC_EnableIRQ(USART1_IRQn);
+		NVIC_SetPriority(USART1_IRQn, 1);
 }
 
 void USART1_IRQHandler(void) // Обработчик прерывания USART1
@@ -175,7 +136,7 @@ void USART1_IRQHandler(void) // Обработчик прерывания USART1
         if (buffer != 0x0D)
         {
             xbuffer[xlen] = buffer;
-            sendChar(buffer); // Отправка прочитанного символа обратно
+            sendChar(buffer); 
             xlen++;
             return;
         }
@@ -193,13 +154,19 @@ void USART1_IRQHandler(void) // Обработчик прерывания USART1
             sendArr(xbuffer);
 
             clearXBuff();
+            sprintf(xbuffer, "%d", counter);
+            sendArr("\nCount: ");
+            sendArr(xbuffer);
+
+            clearXBuff();
             sendArr("\n");
             return;
         }
 
         if (isXBuff("start"))
-        {
-            TIM_Cmd(TIM3, ENABLE);
+        {		
+						if (TimObj.TIM_Period & ChObj.TIM_Pulse)
+							TIM_Cmd(TIM3, ENABLE);
             clearXBuff();
             sendArr("\n");
             return;
@@ -218,6 +185,7 @@ void USART1_IRQHandler(void) // Обработчик прерывания USART1
             TimObj.TIM_Period = getArg();
             TIM_TimeBaseInit(TIM3, &TimObj);
             TIM_OC1Init(TIM3, &ChObj);
+            counter--; // так как инициализация вызывает прерывание IT_Update
             sendArr("\n");
             clearXBuff();
             return;
@@ -228,6 +196,7 @@ void USART1_IRQHandler(void) // Обработчик прерывания USART1
             ChObj.TIM_Pulse = getArg();
             TIM_TimeBaseInit(TIM3, &TimObj);
             TIM_OC1Init(TIM3, &ChObj);
+            counter--; // так как инициализация вызывает прерывание IT_Update
             clearXBuff();
             sendArr("\n");
             return;
@@ -238,15 +207,49 @@ void USART1_IRQHandler(void) // Обработчик прерывания USART1
         return;
     }
 }
+static void initTIM3(void)
+{
+    /* Включение тактирования таймера TIM3 */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+    /* Настройка периода и прескеллера TIM3 */
+    TimObj.TIM_Period = 0;
+    TimObj.TIM_Prescaler = 107;
+
+    /* Настройка делителя частоты, режим счета таймера TIM3 */
+    TimObj.TIM_ClockDivision = 0;                /* Делитель частоты таймера */
+    TimObj.TIM_CounterMode = TIM_CounterMode_Up; /* Режим счета вверх */
+
+    /* Настройка канала 1 в режиме PWM */
+    ChObj.TIM_OCMode = TIM_OCMode_PWM1;             /* Режим работы канала - PWM1 */
+    ChObj.TIM_OutputState = TIM_OutputState_Enable; /* Включение выхода канала */
+    ChObj.TIM_OCPolarity = TIM_OCPolarity_High;     /* Полярность выходного сигнала - прямая */
+    ChObj.TIM_Pulse = TimObj.TIM_Period / 2;
+
+    TIM_OC1Init(TIM3, &ChObj);                 /* Применение настроек к каналу 1 таймера TIM3 */
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE); /* Включение прерывания на канале 1 */
+
+    NVIC_EnableIRQ(TIM3_IRQn);
+		NVIC_SetPriority(TIM3_IRQn, 2);
+
+}
+
+void TIM3_IRQHandler(void)
+{
+    /* Если произошло прерывание по событию захвата на канале 4 */
+    if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
+    {
+        TIM_ClearITPendingBit(TIM3, TIM_IT_Update); /* Сброс флага прерывания */
+        counter++;
+    }
+}
 
 int main(void)
 {
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE); // Включение тактирования DMA1
-    initGPIO();                                        // Инициализация GPIO
-    initUSART();                                       // Инициализация USART
-    initNVIC();                                        // Инициализация NVIC
-    initTIM3                                           // Инициализация TIM3
-        while (1)
+    initGPIO();  // Инициализация GPIO
+    initUSART(); // Инициализация USART
+    initTIM3();  // Инициализация TIM3
+    while (1)
     {
     }
 }
